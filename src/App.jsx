8a74ab59fa433b9c2d03fc7c441f77ceb384dbba
bakarewealth@gmail.com <bakarewealth@gmail.com>
2024-02-26 16:39:39 +0100

    NavBar Implementation

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from './components/NavBar'
+import ToDo from './components/ToDo'
+
+const App = () => {
+  return (
+    <div>
+      <NavBar/>
+      <ToDo/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import bgImage from "../assets/images/bg-mobile-dark.jpg"
+
+const NavBar = () => {
+    return (
+     <nav className='bg-indigo-950 h-screen'>
+      <img src={bgImage} alt="" />
+      <p className="">TODO</p>
+     </nav>
+  )
+}
+
+export default NavBar
